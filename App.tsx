505fbd17e27151ee9e0b178f93f11d3a9bac8e37
Mukesh Maurya <77536282+mukeshmaurya23@users.noreply.github.com>
2024-10-16 16:55:19 +0530

    work on redux toolkit zustand and mmkv

--- App.tsx
+++ App.tsx
@@ -6,20 +6,26 @@
  */
 
 import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
+import { Provider } from 'react-redux';
+import store, { persistAppStore } from './src/redux/store';
+import Home from './src/screens/Home/Home';
+import { PersistGate } from 'redux-persist/integration/react';
+import { customMMKVStorage, MMKVStorage, storage, } from './src/utils/storage-mmkv';
 
 
 function App(): JSX.Element {
-
   console.log("CD_CD_Test2")
-
+  customMMKVStorage.setItem("token", "4678shshshshshsh")
+  // storage.getAllKeys()
+  console.log(storage.getAllKeys())
+  console.log(storage.contains('token'))
+  console.log(storage.getString('token'))
   return (
-    <View style={{ flex: 1 }}>
-      <Text style={{ color: "red" }}>CI_CD_Test</Text>
-    </View>
+    <Provider store={store}>
+      <PersistGate persistor={persistAppStore} loading={null}>
+        <Home />
+      </PersistGate>
+    </Provider>
   );
 }
 
